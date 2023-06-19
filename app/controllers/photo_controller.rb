0e6d5b57e5a0c90b2768c51171bef7a1a867a0a6
kazunari920 <wasd.kazunari@gmail.com>
2023-06-19 08:07:04 +0000

    フラッシュメッセージ日本語化

---
+++ app/controllers/photo_controller.rb
@@ -0,0 +1,4 @@
+class PhotoController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
+  get 'photo/show'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
 
-  root   "static_pages#home"
-  get    "/help",    to: "static_pages#help"
-  get    "/about",   to: "static_pages#about"
-  get    "/flow",    to: "static_pages#flow"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/photo", to: "photo#show"
+  get "/flow", to: "static_pages#flow"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :registrations, only: [:new, :create]
+  resources :users, only: [:show, :index]
 
-
+  get 'users/:id', to: 'users#show', as: 'user_show'
 end
