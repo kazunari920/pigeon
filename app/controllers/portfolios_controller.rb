1db0f9c7efa0812f3e2cfaefaccdd45a93b7fcbf
kazunari920 <wasd.kazunari@gmail.com>
2023-07-06 15:37:23 +0900

    画像アップロード確認

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,28 @@
 class PortfoliosController < ApplicationController
+  def new
+    @photographer = Photographer.find(params[:photographer_id])
+    @portfolio = @photographer.portfolios.build
+  end
+
+  def create
+    @photographer = Photographer.find(params[:photographer_id])
+    @portfolio = @photographer.portfolios.build(portfolio_params)
+
+    if @portfolio.save
+      redirect_to [@photographer, @portfolio]
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @photographer = Photographer.find(params[:photographer_id])
+    @portfolio = @photographer.portfolios.find(params[:id])
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :description, images: [])
+  end
 end
