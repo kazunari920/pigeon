0446b33f144e5ee474f4683f3d0e791073f91414
kazunari920 <wasd.kazunari@gmail.com>
2023-07-14 14:44:30 +0900

    レンダーのhost許可

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,10 @@ Rails.application.configure do
   config.active_storage.service = :amazon
   # 画像の変換にminimagickを使う
   config.active_storage.variant_processor = :mini_magick
+  # renderのホスト許可
+  config.hosts << ".onrender.com"
+  config.hosts << "pigeon-400z.onrender.com"
+
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
