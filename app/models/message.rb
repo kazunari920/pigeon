71f9dd5f6ebadda1577f6a3d43c440ef537d4266
kazunari920 <wasd.kazunari@gmail.com>
2023-08-01 19:16:31 +0900

    userのrequest詳細でステータスによってメッセージを非表示

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,21 +2,11 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :photographer
   belongs_to :request
-  before_create :check_request_status
   enum from: { user: 1, photographer: 10 }
 
   validates :user_id, :photographer_id, :request_id, :content, presence: true
 
   def sender
     self.user? ? user.name : photographer.name
-  end
-
-  private
-
-  def check_request_status
-    return if request.accepted? || request.completed?
-
-    errors.add(:base, 'リクエストが承認されていません。')
-    throw :abort
   end
 end
